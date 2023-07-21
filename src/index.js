988910cfc6f6b68c2ca3f6efa2879e4fa17ece45
belyjnikolaj <117487574+belyjnikolaj@users.noreply.github.com>
2023-07-21 23:01:39 +0300

    Add HW-06

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from 'components/app/App';
 import './index.css';
 import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
+
 import { store, persistor } from 'redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
